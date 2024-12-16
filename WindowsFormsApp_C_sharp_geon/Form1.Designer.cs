87e95c62f3884e84af215dd1b6b51bbdde731a85
Polar-Bear-Poby <primekk3502@gmail.com>
2024-12-16 13:49:28 +0900

    Add project files.

---
+++ WindowsFormsApp_C_sharp_geon/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp_C_sharp_geon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(118, 127);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(544, 225);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp_C_sharp_geon/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_C_sharp_geon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_print.Text = "241216 3교시";
+            byte days_of_80 = 81;
+            // 80일 세계 일주 실제 시간
+            short sfhard_training_session = 6;
+            // 오늘 교육 차수
+            int soldiers_of_spartans = 300;
+            // 영화 300에 나온 스파트타 군대 수
+            float my_height = 180.6f;
+            // 나의 키
+            double korean_war_deaths = 3000000;
+            //한국 전쟁 때 사망자 수
+            decimal us_national_debt = 33000000000000;
+
+            textBox_print.Text = days_of_80.GetType().ToString() + " days_of_80: " + days_of_80 + "\r\n";
+            textBox_print.Text += sfhard_training_session.GetType().ToString() + " sfhard_training_session: " + sfhard_training_session + "\r\n";
+            textBox_print.Text += soldiers_of_spartans.GetType().ToString() + " soldiers_of_spartans: " + soldiers_of_spartans + "\r\n";
+            textBox_print.Text += my_height.GetType().ToString() + " my_height: " + my_height + "\r\n";
+            textBox_print.Text += korean_war_deaths.GetType().ToString() + " korean_war_deaths: " + korean_war_deaths + "\r\n";
+            textBox_print.Text += us_national_debt.GetType().ToString() + " us_national_debt: " + us_national_debt + "\r\n";
+
+
+        }
+    }
+}
