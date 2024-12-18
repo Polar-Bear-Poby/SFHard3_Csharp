92fa3cdfde4aede536e8f795edc04783b4bc8a1d
Polar-Bear-Poby <primekk3502@gmail.com>
2024-12-18 13:03:58 +0900

    20241218 5교시 과제

--- WindowsFormsApp_C_sharp_geon/Practice_1218_5th.Designer.cs
+++ WindowsFormsApp_C_sharp_geon/Practice_1218_5th.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp_C_sharp_geon
 {
-    partial class Practice_1218_3rd
+    partial class Practice_1218_5th
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Practice_1218_3rd_print = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Practice_1218_3rd_print
+            // textBox_result
             // 
-            this.Practice_1218_3rd_print.Location = new System.Drawing.Point(118, 127);
-            this.Practice_1218_3rd_print.Multiline = true;
-            this.Practice_1218_3rd_print.Name = "Practice_1218_3rd_print";
-            this.Practice_1218_3rd_print.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.Practice_1218_3rd_print.Size = new System.Drawing.Size(544, 225);
-            this.Practice_1218_3rd_print.TabIndex = 0;
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(115, 169);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox_result.Size = new System.Drawing.Size(544, 225);
+            this.textBox_result.TabIndex = 2;
             // 
             // label1
             // 
@@ -51,16 +55,35 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Results";
             // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(115, 139);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(463, 28);
+            this.textBox_input.TabIndex = 0;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(584, 139);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "button1";
+            this.button_input.UseVisualStyleBackColor = true;
+            // 
             // Practice_1218_5th
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_input);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Practice_1218_3rd_print);
+            this.Controls.Add(this.textBox_result);
             this.Name = "Practice_1218_5th";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Text = "Pracitce_1218_3rd";
+            this.Text = "Pracitce_1218_5th";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,8 +91,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox Practice_1218_3rd_print;
+        private System.Windows.Forms.TextBox textBox_result;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input;
     }
 }
 
---
+++ WindowsFormsApp_C_sharp_geon/Practice_1218_5th.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_C_sharp_geon
+{
+    public partial class Practice_1218_5th : Form
+    {
+        public Practice_1218_5th()
+        {
+            InitializeComponent();
+            textBox_result.Text = "241218 5교시" + "\r\n";
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
